ba0d5e41d1910af67ad87992459250b407a6037b
Michael Strutt <michael@mstrutt.co.uk>
2015-08-24 11:14:17 +0100

    basic sass compiling with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	sass = require('gulp-sass');
 
-gulp.task('default', function() {
-	// place code for your default task here
+gulp.task('sass', function () {
+	gulp.src('./main.scss')
+		.pipe(sass({
+			indentType: 'tab',
+			indentWidth: 1,
+			outputStyle: 'expanded',
+			sourceComments: true
+		}))
+		.pipe(sass.sync().on('error', sass.logError))
+		.pipe(gulp.dest('./'));
 });
+
+gulp.task('sass:watch', function () {
+	gulp.watch('./main.scss', ['sass']);
+});
+
+gulp.task('default', ['sass:watch']);
