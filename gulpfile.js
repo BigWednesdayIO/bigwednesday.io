d100a93c623f08538d003f9b79eb15115275afb2
Michael Strutt <michael@mstrutt.co.uk>
2015-08-24 13:24:01 +0100

    adding a basic gulp build setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,49 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+	runSequence = require('run-sequence'),
+	gulpLoadPlugins = require('gulp-load-plugins'),
+	plugins = gulpLoadPlugins();
 
 gulp.task('sass', function () {
-	gulp.src('./main.scss')
-		.pipe(sass({
-			indentType: 'tab',
-			indentWidth: 1,
-			outputStyle: 'expanded',
-			sourceComments: true
+	return gulp.src('app/assets/scss/*.scss')
+		.pipe(plugins.sourcemaps.init())
+		.pipe(plugins.sass())
+		.pipe(plugins.autoprefixer({
+			browsers: ['last 3 versions'],
 		}))
-		.pipe(sass.sync().on('error', sass.logError))
-		.pipe(gulp.dest('./'));
+		.pipe(plugins.sourcemaps.write('.'))
+		.pipe(gulp.dest('app/assets/css'));
 });
 
-gulp.task('sass:watch', function () {
-	gulp.watch('./main.scss', ['sass']);
+gulp.task('clean:build', function () {
+	return gulp.src('build', {read: false})
+		.pipe(plugins.clean());
 });
 
-gulp.task('default', ['sass:watch']);
+gulp.task('copy:build', function () {
+	return gulp.src([
+			'app/**',
+			'!app/assets/{css,scss}{,/**}',
+			'!app/index.html'
+		])
+		.pipe(gulp.dest('build/'));
+});
+
+gulp.task('build:assets', function () {
+	return gulp.src('app/index.html')
+		.pipe(plugins.usemin({
+			css: [plugins.minifyCss(), plugins.rev()]
+		}))
+		.pipe(gulp.dest('build/'));
+});
+
+gulp.task('build', function(callback) {
+	runSequence('clean:build',
+		['copy:build', 'build:assets'],
+	callback);
+});
+
+gulp.task('watch', function () {
+	gulp.watch('app/main.scss', ['sass']);
+});
+
+gulp.task('default', ['watch']);
