30c46b4f583d2566eff8e4390f12baaeab7f4f92
Michael Strutt <michael@mstrutt.co.uk>
2015-09-15 12:01:13 +0100

    adding sub-navigation for products

--- app/contents/assets/js/app.js
+++ app/contents/assets/js/app.js
@@ -5,11 +5,38 @@
 
 	document.addEventListener('DOMContentLoaded', function(event) { 
 		var navElement = document.getElementsByClassName('primary-navigation')[0];
+		var subNavElement = document.getElementsByClassName('sub-nav')[0];
+		var productsLink = document.getElementById('products-link');
 
 		if (navElement) {
 			document.getElementById('navicon').addEventListener('click', function() {
 				navElement.classList.toggle('is-hidden');
 			});
+		}
+
+		var timer;
+
+		function showSubNav () {
+			clearTimeout(timer);
+			timer = setTimeout(function() {
+				productsLink.classList.add('is-open')
+				subNavElement.classList.remove('is-hidden');
+			}, 100);
+		}
+
+		function hideSubNav () {
+			clearTimeout(timer);
+			timer = setTimeout(function() {
+				productsLink.classList.remove('is-open')
+				subNavElement.classList.add('is-hidden');
+			}, 100);
+		}
+
+		if (subNavElement) {
+			productsLink.addEventListener('mouseenter', showSubNav);
+			productsLink.addEventListener('mouseleave', hideSubNav);
+			subNavElement.addEventListener('mouseenter', showSubNav);
+			subNavElement.addEventListener('mouseleave', hideSubNav);
 		}
 	});
 })();
